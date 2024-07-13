f6abe25e42614127c6e122ac771c7d870f4d92f5
DivyaChakilam <161245630+DivyaChakilam@users.noreply.github.com>
2024-07-13 16:44:56 -0400

    Food Category code update

---
+++ src/test/java/com/recipescrapers/main/RecipeConstants.java
@@ -0,0 +1,13 @@
+package com.recipescrapers.main;
+
+import java.util.Arrays;
+
+public class RecipeConstants {
+	
+	static String[] eggetarianEliminateOptions = new String[] {"veggie", "eggplant", "without egg","eggless"};
+	static String[] veganEliminateOptions = new String[] {"egg" , "milk" , "honey" ,"butter","cheese","ghee","gelatin"};
+    public static final String LCHFToEliminate = "pork,Meat,Poultry,Fish,Sausage,ham,salami,bacon,cheese,butter,Ice cream,egg,prawn,Oil,olive oil,coconut oil,soybean oil,corn oil,safflower oil,sunflower oil,rapeseed oil,peanut oil,cottonseed oil,canola oil,mustard oil,cereals,tinned vegetable,bread,maida,atta,sooji,poha,cornflake,cornflour,pasta,White rice,pastry,cakes,biscuit,soy,soy milk,white miso paste,soy sauce,soy curls,edamame,soy yogurt,soy nut,tofu,pies,Chip,cracker,potato,sugar,jaggery,glucose,fructose,corn syrup,cane sugar,aspartame,cane solid,maltose,dextrose,sorbitol,mannitol,xylitol,maltodextrin,molasses,brown rice syrup,splenda,nutra sweet,stevia,barley malt,mustard (rai / sarson) oil";
+    
+    	
+
+}
