6fe939202d880581d39936fac9e241327c9b92af
Divya Chakilam <161245630+DivyaChakilam@users.noreply.github.com>
2024-07-14 23:26:28 -0400

    Divya (#18)
    * DB Changes (#9)
    
    * Pagination commit (#3)
    
    * Json
    
    * DatabaseClass
    
    * Mainclass
    
    ---------
    
    Co-authored-by: nehasaini09 <116294426+nehasaini09@users.noreply.github.com>
    Co-authored-by: nehasaini09 <gaurav.saini@htsr-spare-7480-WVF2.ad.healthtell.com>
    
    * LCHF Elimination Code related changes
    
    * LCHF Elimination code related changes (#11) (#12)
    
    * DB Changes (#9)
    
    * Pagination commit (#3)
    
    * Json
    
    * DatabaseClass
    
    * Mainclass
    
    ---------
    
    
    
    
    * LCHF Elimination Code related changes
    
    ---------
    
    Co-authored-by: nehasaini09 <116294426+nehasaini09@users.noreply.github.com>
    Co-authored-by: nehasaini09 <gaurav.saini@htsr-spare-7480-WVF2.ad.healthtell.com>
    
    * LCHF Elimination code related changes (#11) (#15)
    
    * DB Changes (#9)
    
    * Pagination commit (#3)
    
    * Json
    
    * DatabaseClass
    
    * Mainclass
    
    ---------
    
    
    
    
    * LCHF Elimination Code related changes
    
    ---------
    
    Co-authored-by: nehasaini09 <116294426+nehasaini09@users.noreply.github.com>
    Co-authored-by: nehasaini09 <gaurav.saini@htsr-spare-7480-WVF2.ad.healthtell.com>
    
    * LFV code
    
    ---------
    
    Co-authored-by: nehasaini09 <116294426+nehasaini09@users.noreply.github.com>
    Co-authored-by: nehasaini09 <gaurav.saini@htsr-spare-7480-WVF2.ad.healthtell.com>

--- src/test/java/com/recipescrapers/main/Recipe.java
+++ src/test/java/com/recipescrapers/main/Recipe.java
@@ -2,14 +2,79 @@ package com.recipescrapers.main;
 
 public class Recipe {
 	
-	private String recipeID,recipeName,ingredients;
-	/*
-	 * Recipe Category(Breakfast/lunch/snack/dinner) Food
-	 * Category(Veg/non-veg/vegan/Jain)
-	 * 
-	 * Preparation Time Cooking Time Tag No of servings Cuisine category Recipe
-	 * Description Preparation method Nutrient values Recipe URL
-	 */
+	private String recipeID,recipeName,ingredients,recipeDescription,preparationMethod,nutritionValues,recipeUrl,preperationTime,cookingTime,numOfServings,foodCategory,ingredientsName;
+	private boolean lfvRecipesToAvoid;
+	public String getIngredientsName() {
+		return ingredientsName;
+	}
+
+	public void setIngredientsName(String ingredientsName) {
+		this.ingredientsName = ingredientsName;
+	}
+
+	public String getFoodCategory() {
+		return foodCategory;
+	}
+
+	public void setFoodCategory(String foodCategory) {
+		this.foodCategory = foodCategory;
+	}
+
+	public String getPreperationTime() {
+		return preperationTime;
+	}
+
+	public String getNumOfServings() {
+		return numOfServings;
+	}
+
+	public void setNumOfServings(String numOfServings) {
+		this.numOfServings = numOfServings;
+	}
+
+	public void setPreperationTime(String preperationTime) {
+		this.preperationTime = preperationTime;
+	}
+
+	public String getCookingTime() {
+		return cookingTime;
+	}
+
+	public void setCookingTime(String cookingTime) {
+		this.cookingTime = cookingTime;
+	}
+
+	public String getRecipeDescription() {
+		return recipeDescription;
+	}
+
+	public void setRecipeDescription(String recipeDescription) {
+		this.recipeDescription = recipeDescription;
+	}
+
+	public String getPreparationMethod() {
+		return preparationMethod;
+	}
+
+	public void setPreparationMethod(String preparationMethod) {
+		this.preparationMethod = preparationMethod;
+	}
+
+	public String getNutritionValues() {
+		return nutritionValues;
+	}
+
+	public void setNutritionValues(String nutritionValues) {
+		this.nutritionValues = nutritionValues;
+	}
+
+	public String getRecipeUrl() {
+		return recipeUrl;
+	}
+
+	public void setRecipeUrl(String recipeUrl) {
+		this.recipeUrl = recipeUrl;
+	}
 
 	public String getRecipeID() {
 		return recipeID;
@@ -36,8 +101,21 @@ public class Recipe {
 	}
 	
 	public String toString() {
-		return recipeName;	
+		return "ReceipeName :"+ recipeName + "Ingredenients :" + ingredients;
 	}
+
+	public boolean isLfvRecipesToAvoid() {
+		return lfvRecipesToAvoid;
+	}
+
+	public void setLfvRecipesToAvoid(boolean lfvRecipesToAvoid) {
+		this.lfvRecipesToAvoid = lfvRecipesToAvoid;
+	}
+	
+	
+	
+	
+
 	
 	
 }
